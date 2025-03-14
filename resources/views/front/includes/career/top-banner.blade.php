75e34443defa7870c7c18177812804610f0a4e8a
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-14 08:47:44 +0545

    feat: implement top banner section with image slider and explore button

--- resources/views/front/includes/career/top-banner.blade.php
+++ resources/views/front/includes/career/top-banner.blade.php
@@ -1,1 +1,32 @@
-top-banner
+<section class="top-banner">
+    <div class="main-container">
+        <div class="slider" id="slick-slider">
+            <div class="image-card">
+                <img class="img-fluid" src="{{ asset('front/img/career/slide-banner-1.jpg') }}" alt="Slider Image">
+
+            </div>
+            <div class="image-card">
+                <img class="img-fluid" src="{{ asset('front/img/career/slide-banner-2.jpg') }}" alt="Slider Image">
+
+            </div>
+            <div class="image-card">
+                <img class="img-fluid" src="{{ asset('front/img/career/slide-banner-3.png') }}" alt="Slider Image">
+
+            </div>
+        </div>
+        <button class="explore-btn">Explore Jobs</button>
+    </div>
+</section>
+
+@push('js')
+    <script>
+        $(document).ready(function() {
+            $('#slick-slider').slick({
+                autoplay: true,
+                autoplaySpeed: 2000,
+                arrows: false,
+                dots: true
+            })
+        })
+    </script>
+@endpush
