7a256e638248d63a4f97f2df3aae1e8e1f33a538
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-16 11:36:56 +0545

    feat: add 'Not Sure' section with responsive design, styling, and resume submission button

--- resources/views/front/includes/career/not-sure.blade.php
+++ resources/views/front/includes/career/not-sure.blade.php
@@ -1,1 +1,14 @@
-not-sure
+<section class="not-sure">
+        <div class="banner-para">
+            <h1 class="banner-title">
+                Still Searching for the Right Position?
+            </h1>
+            <p>Let's Find Your Fit!</p>
+            <button class="resume">Submit Your Resume</button>
+        </div>
+        <div class="banner-img">
+            <picture>
+                <img src="{{ asset('front/img/career/not-sure.jpg') }}" alt="Not Sure">
+            </picture>
+        </div>
+</section>
