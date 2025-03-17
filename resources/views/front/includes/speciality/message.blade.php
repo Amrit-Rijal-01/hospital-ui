3fe4d73415b10d6221ed0b82b9b99a0fcc8f196c
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-17 13:07:54 +0545

    feat: add message section with responsive design, image content, and styling enhancements

--- resources/views/front/includes/speciality/message.blade.php
+++ resources/views/front/includes/speciality/message.blade.php
@@ -1,1 +1,20 @@
-message
+<section class="message">
+    <div class="main-container">
+        <div class="content d-flex flex-xl-row flex-column">
+            <div class="para align-self-center fst-italic">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ducimus odit ullam dolor? Sit vel est odit quasi facilis quos reprehenderit eaque deserunt alias quo, deleniti laudantium modi itaque saepe aspernatur, temporibus quia soluta a repudiandae ipsum perferendis! Repudiandae qui nihil harum at? Vel ea nulla earum, facere maiores repellat?
+            </div>
+            <div class="img-block align-self-center text-center">
+                <img src="{{ asset('front/img/speciality/chairman.png') }}" alt="Chairman Image">
+            </div>
+            <div class="detail align-self-center text-center">
+                <div class="heading-lg name">
+                    Dr. Naresh Shah
+                </div>
+                <div class="heading-sm role">
+                    Chairman & Managing Director
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
