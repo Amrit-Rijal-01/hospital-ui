aec56251c87c8489cb1a57873a493171b63de53c
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-17 09:28:50 +0545

    feat: update heading styles across multiple sections and add resume submission modal

--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -103,10 +103,10 @@
                 <span>Emergency</span>
             </a>
 
-            <a href="#" class="call-back navbar-link">
+            <button href="#" class="call-back navbar-link" data-bs-toggle="modal" data-bs-target="#resume-modal">
                 <img src="{{ asset('front/img/phone-icon.png') }}" alt="Call Back" class="call-back-image me-1">
                 <small>Request Call Back</small>
-            </a>
+            </button>
             <a href="#" class="md-contact-us">
                 <img src="{{ asset('front/img/phone-grey-icon.png') }}" alt="Call Back" class="call-back-image me-1">
                 <span>Contact</span>
@@ -114,6 +114,43 @@
         </div>
     </div>
 </header>
+<div class="modal fade " id="resume-modal">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content p-4">
+                <div class="modal-header p-0 pb-3 border-bottom-0">
+                     <h2 class="modal-title heading-md text-center">Request Call Back</h2>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="">
+                    @csrf
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" name="name" placeholder="Full Name *" required>
+                                <label for="name">Full Name *</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-floating mb-3">
+                                <input type="tel" class="form-control" name="phoneNumber" placeholder="Phone Number *" required>
+                                <label for="phoneNumber">Phone Number *</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" name="email" placeholder="Email Address *" required>
+                                <label for="email" >Email
+                                    Address *</label>
+                            </div>
+                        </div>
+                            <div class="col-12 submit-btn">
+                                <button class="w-100" id="submit-callback">Submit</button>
+                            </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @push('js')
     <script>
             function extendSubMenu(el) {
