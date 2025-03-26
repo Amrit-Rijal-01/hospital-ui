b053c402d68ca332cd4aa8a91a907dbb0989e359
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-26 14:34:29 +0545

    feat: add infrastructure section with responsive design and content

---
+++ resources/views/front/includes/why-us/infrastructure.blade.php
@@ -0,0 +1,38 @@
+<section id="infrastructure">
+   <div class="main-container">
+      <div class="heading mb-4 text-center">
+         World Class Infrastructure
+      </div>
+      <div class="content">
+         <div class="para-wrap">
+            <p>
+               Medanta provides doctors a platform with state-of-the-art infrastructure, enabling them to perform complex procedures with ease. For patients, Medanta hospitals offer a safe and efficient environment that facilitates fast recovery. The hospitals are equipped with a high proportion of operating theatres (OTs) and intensive care units (ICUs), reflecting the focus on handling complex clinical work. The buildings are designed with a strong emphasis on patient safety, adhering to the highest infection control practices. To ensure this, dedicated Air Handling Units (AHUs) are installed for each OT, ICU, and ward in each of our facility to maintain optimal air quality and minimizing the risk of infections.
+            </p>
+         </div>
+         <div class="img-wrapper">
+            <div class="row g-4">
+                  <div class="col-6">
+                    <div class="image rounded-4 overflow-hidden">
+                      <img src="{{ asset('front/img/why-us/infrastructure.jpg') }}" alt="Infrastructure Image" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                  </div>
+                  <div class="col-6">
+                    <div class="image  rounded-4 overflow-hidden">
+                      <img src="{{ asset('front/img/why-us/infrastructure.jpg') }}" alt="Infrastructure Image" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                  </div>
+                  <div class="col-6">
+                    <div class="image rounded-4 overflow-hidden">
+                      <img src="{{ asset('front/img/why-us/infrastructure.jpg') }}" alt="Infrastructure Image" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                  </div>
+                  <div class="col-6">
+                    <div class="image rounded-4 overflow-hidden">
+                      <img src="{{ asset('front/img/why-us/infrastructure.jpg') }}" alt="Infrastructure Image" class="img-fluid w-100 object-fit-cover">
+                    </div>
+                  </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
