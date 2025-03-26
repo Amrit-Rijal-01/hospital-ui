cd2c23671206fbae374e63f307b7efcec3ba08a6
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-26 15:21:40 +0545

    feat: add care section with background image and content layout

---
+++ resources/views/front/includes/why-us/care.blade.php
@@ -0,0 +1,10 @@
+<section id="care">
+   <div class="main-container">
+      <div class="content">
+         <div class="heading">Caring Systems And Processes</div>
+         <div class="para-wrap">
+            Nobel leverages advanced and innovative digital solutions to enhance the accessibility and quality of care provided to patients. These solutions enable seamless access to healthcare services and allow Nobel's expertise to reach the maximum possible people. The company has also implemented uniform quality guidelines and standards across specialities and units to ensure that care is delivered in a safe and effective manner keeping the doctor-patient relationship at the centre of everything that we do.
+         </div>
+      </div>
+   </div>
+</section>
