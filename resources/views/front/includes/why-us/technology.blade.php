8ed946f54293d50dc9dd2a1f959c89f811ec0033
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-26 14:50:49 +0545

    feat: add technology section with responsive card layout and slider component

---
+++ resources/views/front/includes/why-us/technology.blade.php
@@ -0,0 +1,31 @@
+<section id="technology">
+    <div class="main-container">
+        <x-sliderComponent heading="Latest High-end Technology"
+            subHeading="Medanta is dedicated to delivering high-end tertiary and quaternary care using cutting-edge technology and equipment. Medanta has provided the latest tools to its doctors so they can offer the most complex diagnostics and treatment using latest technology. Examples of this include a hybrid OT-Cath Lab, Brain Suite (MRI inside an OT), multiple robotic surgical systems, latest precision radiation oncology machines such as Varian EDGE and Cyberknife, as well as the highestend of diagnostic equipment.">
+            <div class="each-card">
+               <div class="img-wrapper">
+                  <img src="{{ asset('front/img/why-us/technology.png') }}" alt="Technology Image">
+               </div>
+               <div class="body">
+                  <div class="heading-sm">Technology Name</div>
+               </div>
+            </div>
+            <div class="each-card">
+               <div class="img-wrapper">
+                  <img src="{{ asset('front/img/why-us/technology.png') }}" alt="Technology Image">
+               </div>
+               <div class="body">
+                  <div class="heading-sm">Technology Name</div>
+               </div>
+            </div>
+            <div class="each-card">
+               <div class="img-wrapper">
+                  <img src="{{ asset('front/img/why-us/technology.png') }}" alt="Technology Image">
+               </div>
+               <div class="body">
+                  <div class="heading-sm">Technology Name</div>
+               </div>
+            </div>
+        </x-sliderComponent>
+    </div>
+</section>
