9753a04ce145915876fbe0e6055ed262f30e2a5d
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-26 07:36:32 +0545

    feat: update benefit-risk sections for technology and treatment with new content

--- resources/views/front/includes/per-treatment/benefit-risk.blade.php
+++ resources/views/front/includes/per-treatment/benefit-risk.blade.php
@@ -1,5 +1,6 @@
 <section id="benefit-risk">
    <div class="main-container">
-      <x-type2 heading="What are the benefits and risks of Abdominoplasty procedure?" subHeading="As any other reconstructive surgery, abdominoplasty comes with many advantages and just a few disadvantages, which a person must be aware of."></x-type2>
+      <x-type2 heading="What Are The Benefits & Risks Of This Technology?"
+      subHeading="CT Scan has several advantages over the traditional methods of radiography. CT Scan on Wheels completely eliminates the superimposition of images, although the process comes with its own set of risks which the patient should be aware of."></x-type2>
    </div>
 </section>
