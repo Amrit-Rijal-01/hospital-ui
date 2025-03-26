bd8b3aec3c9fdfa8d437a8562948a3f285cddb4a
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-26 15:37:05 +0545

    feat: add trust section with content layout and responsive styles

---
+++ resources/views/front/includes/why-us/trust.blade.php
@@ -0,0 +1,22 @@
+<section id="trust">
+    <div class="main-container">
+        <div class="heading text-center mb-4">
+            Trust-based Compassionate Care
+        </div>
+        <div class="content d-flex flex-nowrap flex-column flex-lg-row">
+            <div class="img-wrapper">
+                <img src="{{ asset('front/img/why-us/trust.jpg') }}" alt="Trust Image">
+            </div>
+            <div class="body align-self-center">
+                <div class="para-wrap">
+                    Nobel is committed to delivering advanced healthcare by establishing institutes of excellence that
+                    seamlessly integrate medical care, teaching, and researched services. This holistic approach ensures
+                    that patients receive the highest standard of care with a focus on individualized treatment plans
+                    tailored to meet their specific needs. Furthermore, Nobel prioritizes transparency in its systems
+                    and processes, allowing patients and their families to have a clear understanding of the care they
+                    receive, including treatment options, costs, and post-operative care plans.
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
