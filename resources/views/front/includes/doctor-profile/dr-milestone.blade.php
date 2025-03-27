6d4a58de23e6d2b7fc937ac82b911116a9acc6af
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-27 23:34:02 +0545

    feat: add milestone section to doctor profile with card layout and styling

---
+++ resources/views/front/includes/doctor-profile/dr-milestone.blade.php
@@ -0,0 +1,21 @@
+<section id="dr-milestone">
+   <div class="main-container">
+      <x-sliderComponent heading="Milestone Achieved">
+         <div class="each-card">
+               <i class="bi bi-check2-circle"></i>
+               <div class="heading-sm">Milestone heading comes here</div>
+               <div class="year">2020</div>
+         </div>
+         <div class="each-card">
+               <i class="bi bi-check2-circle"></i>
+               <div class="heading-sm">Milestone heading comes here</div>
+               <div class="year">2020</div>
+         </div>
+         <div class="each-card">
+               <i class="bi bi-check2-circle"></i>
+               <div class="heading-sm">Milestone heading comes here</div>
+               <div class="year">2020</div>
+         </div>
+      </x-sliderComponent>
+   </div>
+</section>
