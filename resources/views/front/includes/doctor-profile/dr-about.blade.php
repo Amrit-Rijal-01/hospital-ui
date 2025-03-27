d39c1cf189bcdeef0f15d98f718fb2411b3703b0
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-27 23:17:37 +0545

    feat: add about and specialization sections to doctor profile with responsive styling

---
+++ resources/views/front/includes/doctor-profile/dr-about.blade.php
@@ -0,0 +1,23 @@
+<section id="dr-about">
+    <div class="main-container">
+        <div class="heading mb-4">About Dr. Name</div>
+        <div class="row mb-3">
+            <div class="each-col col-md-4">
+                Highest number of Whipple's Surgery in India
+            </div>
+            <div class="each-col col-md-4">
+                Started first department of surgical gastroenterology in India
+            </div>
+            <div class="each-col col-md-4">
+                Fellow of Royal College of Surgeons (FRCS) of Edinburgh
+            </div>
+        </div>
+        <div class="para-wrap">
+            Small Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem aspernatur facilis rerum
+            quos. Eligendi accusamus obcaecati porro magnam harum laudantium adipisci cupiditate eaque, distinctio
+            beatae perferendis commodi ab? Sapiente corrupti mollitia suscipit labore, delectus cumque autem soluta
+            iusto eum inventore voluptatum voluptates obcaecati laborum facere natus. Voluptatibus iste officiis
+            cupiditate?
+        </div>
+    </div>
+</section>
---
+++ resources/views/front/includes/doctor-profile/dr-specialization.blade.php
@@ -0,0 +1,36 @@
+<section id="dr-specialization">
+   <div class="main-container">
+      <x-sliderComponent heading="Specialization & Expertise">
+         <div class="each-card">
+            <a href="#">
+               <div class="img-wrapper d-flex justify-content-center">
+                  <img src="{{ asset('front/img/heart.svg') }}" alt="Treatment Image">
+               </div>
+               <div class="body">
+                  This is for testing
+               </div>
+            </a>
+         </div>
+         <div class="each-card">
+            <a href="#">
+               <div class="img-wrapper d-flex justify-content-center">
+                  <img src="{{ asset('front/img/heart.svg') }}" alt="Treatment Image">
+               </div>
+               <div class="body">
+                  This is for testing
+               </div>
+            </a>
+         </div>
+         <div class="each-card">
+            <a href="#">
+               <div class="img-wrapper d-flex justify-content-center">
+                  <img src="{{ asset('front/img/heart.svg') }}" alt="Treatment Image">
+               </div>
+               <div class="body">
+                  This is for testing
+               </div>
+            </a>
+         </div>
+      </x-sliderComponent>
+   </div>
+</section>
