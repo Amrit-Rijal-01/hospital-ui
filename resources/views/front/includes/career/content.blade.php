a43d48ef7b865188656d46de81c636d41247abaa
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-27 22:37:17 +0545

    feat: enhance employee section with tabbed navigation and philosophy content

--- resources/views/front/includes/career/content.blade.php
+++ resources/views/front/includes/career/content.blade.php
@@ -1,13 +1,13 @@
 <section class="employee-section">
     <div class="main-container">
-        <div class="heading text-center">
+        <div class="heading mb-4 text-center">
             Empowering Lives, Inspiring Excellence
         </div>
         <div class="tabs text-center">
-            <button class="tab active-tab" onclick="changeTab(this)">Employees & Awards</button>
-            <button class="tab" onclick="changeTab(this)">Our Philosophy</button>
+            <button class="tab active-tab" onclick="changeTab(this)" data-tab="#tab-1">Employees & Awards</button>
+            <button class="tab" onclick="changeTab(this)" data-tab="#tab-2">Our Philosophy</button>
         </div>
-        <div class="content-main">
+        <div class="content-main active" id="tab-1">
             <div class="employee-flex d-flex justify-content-between flex-nowrap flex-lg-row flex-column">
                 <div class="employee">
                     <div class="heading-sm mb-4">
@@ -21,8 +21,10 @@
                             <div class="body">
                                 <h3 class="heading">Name</h3>
                                 <p class="card-content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est
-                                    ipsam tempore dolorum nisi provident consequatur? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est
-                                    ipsam tempore dolorum nisi provident consequatur? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est
+                                    ipsam tempore dolorum nisi provident consequatur? Lorem ipsum dolor sit, amet
+                                    consectetur adipisicing elit. Est
+                                    ipsam tempore dolorum nisi provident consequatur? Lorem ipsum dolor sit, amet
+                                    consectetur adipisicing elit. Est
                                     ipsam tempore dolorum nisi provident consequatur?</p>
                             </div>
                         </div>
@@ -109,6 +111,26 @@
                 </div>
             </div>
         </div>
+        <div class="content-main" id="tab-2">
+            <div class="philosophy">
+                <div class="img-wrapper">
+                    <img src="{{ asset('front/img/health-library/blog-placeholder.jpg') }}" alt="Philosophy Image">
+                </div>
+                <div class="content">
+                    <div class="heading mb-4">
+                        Our Philosophy
+                    </div>
+                    <div class="para-wrap">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ipsam? Dignissimos quia ipsam
+                        soluta deleniti eum. Ipsam, provident minus omnis amet, libero inventore quia porro voluptatibus
+                        blanditiis modi voluptate perferendis debitis. Accusantium quasi tenetur voluptas quia delectus
+                        dignissimos suscipit corporis consequatur porro? Pariatur, dolorem provident doloremque esse
+                        velit
+                        minima aperiam.
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
@@ -165,6 +187,12 @@
 
             $('.tab').removeClass('active-tab');
             $(el).addClass('active-tab');
+
+            $('.content-main').removeClass('active');
+            let tabId = $(el).data('tab');
+            console.log(tabId);
+
+            $(tabId).addClass('active');
         }
     </script>
 @endpush
