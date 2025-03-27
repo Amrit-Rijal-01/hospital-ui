8b8358eb0393b744dfc3473b524da9366c9b3671
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-28 00:08:47 +0545

    feat: add article section to doctor profile with card layout and styling

---
+++ resources/views/front/includes/doctor-profile/dr-article.blade.php
@@ -0,0 +1,23 @@
+<section id="dr-article">
+    <div class="main-container">
+      <x-sliderComponent heading="Articles">
+         <a class="article-card" href="/per-blog">
+             <div class="img-wrapper">
+                 <img class="w-100 img-fluid" src="{{ asset('front/img/health-library/health-library-1.jpg') }}"
+                     alt="Doctor">
+             </div>
+             <div class="body d-flex flex-column justify-content-center p-4">
+                 <div class="date mb-3">02 March, 2025</div>
+                 <p class="heading-sm">
+                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt voluptatibus perspiciatis
+                     asperiores quia velit sit.
+                 </p>
+                 <small>By <strong>Dr Name</strong></small>
+             </div>
+         </a>
+      </x-sliderComponent>
+        <div class="all-btn">
+            <x-hoverBtn>View All Article</x-hoverBtn>
+        </div>
+    </div>
+</section>
