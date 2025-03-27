c4d8d6b590b8bf3b5530f2d488f21beab18bdf18
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-28 00:19:37 +0545

    feat: add media section to doctor profile with video thumbnails and styling

---
+++ resources/views/front/includes/doctor-profile/dr-media.blade.php
@@ -0,0 +1,34 @@
+<section id="dr-media">
+    <div class="main-container">
+        <x-sliderComponent heading="Media">
+            <a href="https://www.youtube.com/watch?v=cOfR9N-mcY0" class="glightbox col-xl-4 col-xxl-3">
+                <div class="each-video m-3">
+                    <img src="{{ asset('front/img/video-thumb.jpg') }}" alt="Thumbnail">
+                    <img class="play-icon" src="{{ asset('front/img/play-icon.png') }}" alt="Play Icon">
+                    <div class="heading-sm">Heading of the video</div>
+                </div>
+            </a>
+            <a href="https://www.youtube.com/watch?v=cOfR9N-mcY0" class="glightbox col-xl-4 col-xxl-3">
+                <div class="each-video m-3">
+                    <img src="{{ asset('front/img/video-thumb.jpg') }}" alt="Thumbnail">
+                    <img class="play-icon" src="{{ asset('front/img/play-icon.png') }}" alt="Play Icon">
+                    <div class="heading-sm">Heading of the video</div>
+                </div>
+            </a>
+        </x-sliderComponent>
+        <div class="all-btn">
+            <x-hoverBtn>View All Media</x-hoverBtn>
+        </div>
+    </div>
+</section>
+@push('js')
+    <script>
+        $(document).ready(function() {
+            const lightBox = GLightbox({
+                selector: '.glightbox',
+                touchNavigation: true,
+                loop: true
+            });
+        })
+    </script>
+@endpush
