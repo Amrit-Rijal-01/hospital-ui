4267d909992424f32dbec938c22404210986bc3e
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-29 05:10:34 +0545

    feat: add CPR statistics section with styling and layout

---
+++ resources/views/front/includes/cpr/statistics.blade.php
@@ -0,0 +1,20 @@
+<section id="cpr-stats">
+   <div class="main-container">
+      <x-sliderComponent>
+         <div class="each-card">
+            <i class="bi bi-telephone-outbound mb-4"></i>
+            <div class="heading-sm">
+               <div class="number">2000</div>
+               <div class="text">Trained</div>
+            </div>
+         </div>
+         <div class="each-card">
+            <i class="bi bi-activity mb-4"></i>
+            <div class="heading-sm">
+               <div class="number">2000</div>
+               <div class="text">Trained</div>
+            </div>
+         </div>
+      </x-sliderComponent>
+   </div>
+</section>
