881743ee9487b8ee208b92dcd0155e71c981aff2
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-29 05:29:41 +0545

    feat: add "Why Learn CPR?" section with responsive layout and video

---
+++ resources/views/front/includes/cpr/why-learn-cpr.blade.php
@@ -0,0 +1,21 @@
+<section id="why-learn">
+    <div class="main-content">
+        <div class="video-wrapper">
+            <iframe width="100%" height="515" src="https://youtube.com/embed/rXcm9WvHvCk?si=Y5tg6HtO105Zzr_j" title="CPR"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+            </iframe>
+        </div>
+        <div class="para-wrap">
+            <div class="heading text-center mb-4">Why Learn CPR?</div>
+            <p>
+                Cardiopulmonary resuscitation, also known as CPR, is an emergency procedure which involves performing
+                chest compressions often combined with artificial ventilation in an effort to revive breathing and
+                restore oxygen flow to the brain and vital organs. This is done manually to preserve intact brain
+                function until further medical assistance is available to restore spontaneous blood circulation in a
+                person who is in cardiac arrest.
+            </p>
+        </div>
+    </div>
+</section>
