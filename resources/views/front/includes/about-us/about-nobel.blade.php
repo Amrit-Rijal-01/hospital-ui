4e2df8b22c3c437886dbca22f29e85c13d77846b
Amrit Rijal <rijalamrit720@gmail.com>
2025-03-29 12:54:29 +0545

    feat: add About Nobel section with responsive layout and styling adjustments

---
+++ resources/views/front/includes/about-us/about-nobel.blade.php
@@ -0,0 +1,17 @@
+<section id="about-nobel">
+   <div class="main-container">
+      <div class="d-flex flex-column flex-lg-row gap-4">
+         <div class="img-wrap">
+            <img src="{{ asset('front/img/health-library/blog-placeholder.jpg') }}" alt="About Nobel Image">
+         </div>
+         <div class="content">
+            <div class="heading mb-4">
+               About Nobel
+            </div>
+            <div class="para-wrap">
+               Nobel one of the country’s largest multi-speciality tertiary care provider, mission to deliver advanced but affordable medical services to patients. For many years, Nobel ranked as one of the best private hospital in Nepal.
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
