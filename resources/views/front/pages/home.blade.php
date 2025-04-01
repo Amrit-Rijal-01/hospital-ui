e4272b21d9d98a31d23472bbb619536da99eb712
Amrit Rijal <rijalamrit720@gmail.com>
2025-04-01 16:40:35 +0545

    feat: add homepage modal with responsive styling and automatic display on load

--- resources/views/front/pages/home.blade.php
+++ resources/views/front/pages/home.blade.php
@@ -4,6 +4,19 @@
 @section('meta', 'Home page for the website')
 
 @section('content')
+    <div class="modal fade" id="homepageModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header border-bottom-0">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img class="w-100 img-fluid" src="{{ asset('front/img/modal-test.jpg') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
     @include('front.includes.home.top-banner')
     @include('front.includes.home.speciality')
     @include('front.includes.home.team-section')
@@ -15,3 +28,13 @@
     @include('front.includes.home.award')
     @include('front.includes.home.events')
 @endsection
+
+@push('js')
+    <script>
+        $(document).ready(function() {
+            setTimeout(function() {
+                $("#homepageModal").modal("show");
+            }, 500);
+        });
+    </script>
+@endpush
