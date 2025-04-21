6d75e55aaa861c237cd994f3af39b343ca26c55e
Amrit Rijal <rijalamrit720@gmail.com>
2025-04-21 22:38:08 +0545

    Refactor About Us page: comment out sections for Nobel, Value, and Award

--- resources/views/front/pages/about-us.blade.php
+++ resources/views/front/pages/about-us.blade.php
@@ -4,9 +4,11 @@
 @section('meta', 'About Us page for the website')
 
 @section('content')
-   @include('front.includes.about-us.about-us-banner')
-   @include('front.includes.about-us.about-nobel')
-   @include('front.includes.about-us.about-mission')
-   @include('front.includes.about-us.about-value')
-   @include('front.includes.about-us.about-award')
+    @include('front.includes.about-us.about-us-banner')
+    {{-- @include('front.includes.about-us.about-nobel') --}}
+    @include('front.includes.about-us.about-mission')
+    @include('front.includes.about-us.about-mission')
+    @include('front.includes.about-us.about-mission')
+    {{-- @include('front.includes.about-us.about-value') --}}
+    {{-- @include('front.includes.about-us.about-award') --}}
 @endsection
