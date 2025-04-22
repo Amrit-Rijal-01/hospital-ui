4ff71dede068e2138513170efd1be8c9831e2048
Amrit Rijal <rijalamrit720@gmail.com>
2025-04-22 13:19:37 +0545

    Refactor code structure for improved readability and maintainability

---
+++ resources/views/front/includes/single-service/second-opinion-qn.blade.php
@@ -0,0 +1,23 @@
+<section id="single-service-qn">
+    <div class="main-container">
+        <div class="heading text-center text-capitalize mb-4">
+            Understand When To Take This Service
+        </div>
+        <div class="qn-body">
+            <div class="qn-list">
+                <div class="heading-sm list-heading">
+                    Heading of the list
+                </div>
+                <ul>
+                    <li>Diagnosed with a critical ailment</li>
+                    <li>Doctors have suggested an invasive surgery</li>
+                    <li>If the recommended treatment is risky, invasive or has lifelong consequences</li>
+                </ul>
+            </div>
+            <div class="qn-picture"> {{-- image aspect ratio: 4:2 --}}
+                <img src="{{ asset('front/img/second-opinion.jpg') }}" height="400" width="800"
+                    alt="Second Opinion Image">
+            </div>
+        </div>
+    </div>
+</section>
